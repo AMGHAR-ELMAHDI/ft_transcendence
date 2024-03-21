7e06065b6d19b2b38b04e97d108baddb14b0004c
Elmahdi Amghar <eamghar@e2r7p14.1337.ma>
2024-03-21 16:34:26 +0000

    almost done with profile part of the dashboard

--- React/src/components/Profile.tsx
+++ React/src/components/Profile.tsx
@@ -3,26 +3,31 @@ import React from 'react'
 function Profile() {
   return (
 	<>
-		<div id="profile-usr">
-			<div id="profile-img"><img id="profile-img" src="/bacharG.svg" alt="profilePic"/></div>
-			<h1 id="user-name">Othman Chekairi</h1>
-		</div>
-		<div className="line1">
-			<div className="line2">line2</div>
+		<div className="profile-left">
+			<div id="profile-usr">
+				<img id='profile-img' src="/bacharG.svg" alt="profilePic"/>
+				<h1 id="user-name">Othman Chekairi</h1>
+			</div>
+			<div className="line1">
+				<div className="line2"></div>
+			</div>
 		</div>
 
-		<div className="profile-level">
-			<div id="profile-level-text">
-				<h2>Level 10</h2>
-				<h2>700/1000</h2>
+		<div className="profile-right">
+			<div className="profile-level">
+				<div id="profile-level-text">
+					<h2>Level 10</h2>
+					<h2>700/1000</h2>
+				</div>
+				<div id="profile-level-bar">
+					<progress id="progress-bar" value={75} max={100}/>
+				</div>
 			</div>
-			<div id="profile-level-bar">
-				<progress id="progress-bar" value={75} max={100}/>
+
+			<div id="circles">
+				<div className="progress-circle" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100}></div>
+				<div className="progress-circle" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100}></div>
 			</div>
-		</div>
-		<div id="circles">
-			<div className="progress-circle" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100}></div>
-			<div className="progress-circle" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100}></div>
 		</div>
 	</>
   )
