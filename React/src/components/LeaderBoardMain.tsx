c94fe357783a9c1d524d7efe2be764352ce774b1
Elmahdi Amghar <eamghar@e2r7p14.1337.ma>
2024-04-21 16:51:49 +0100

    done with routing

--- React/src/components/LeaderBoardMain.tsx
+++ React/src/components/LeaderBoardMain.tsx
@@ -11,12 +11,12 @@ function getTop3() {
       </div>
       <div className="SecondThird">
         <div className="Second">
-          <img className="topImgs" src={LeaderBoardData.LeaderBoard[1].picture} alt="FirstPic" />
+          <img className="topImgs" src={LeaderBoardData.LeaderBoard[1].picture} alt="SecondPic" />
           <h1 className="toph1">{LeaderBoardData.LeaderBoard[1].username}</h1>
           <div className="Coins"><h2>100 Coins</h2></div>
         </div>
         <div className="Third">
-          <img className="topImgs" src={LeaderBoardData.LeaderBoard[2].picture} alt="FirstPic" />
+          <img className="topImgs" src={LeaderBoardData.LeaderBoard[2].picture} alt="ThirdPic" />
           <h1 className="toph1">{LeaderBoardData.LeaderBoard[2].username}</h1>
           <div className="Coins"><h2>50 Coins</h2></div>
         </div>
@@ -32,14 +32,20 @@ function getTheRest() {
       <ul>
         {LeaderBoardData.LeaderBoard.map((user) => (
           <li key={user.id} id={user.id.toString()} className={user.id % 2 == 0 ? "SpecialCase" : ""}>
-            <div className="idAndLine">
-              <h1>{user.id}</h1>
-              <div className="lineLi"/>
+            <div className="LeaderRestLeft">
+              <div className="idAndLine">
+                <h1>{user.id}</h1>
+                <div className="lineLi"/>
+              </div>
+              <img className="RestImgs" src={user.picture} alt="userPic" />
             </div>
-            <img className="RestImgs" src={user.picture} alt="userPic" />
-            <h1>{user.username}</h1>
-            <h1>{user.level}</h1>
-            <br />
+            <div className="LeaderRestRight">
+              <div className="LeaderRestRighUsrtName"><h1>{user.username}</h1></div>
+              <div className="LeaderRestRightLvl">
+                <h1>{user.games_won}</h1>
+                <h1>{user.level}</h1>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
