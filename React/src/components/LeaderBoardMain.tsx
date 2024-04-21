a49b8e0f87ef0e4c6832958ba553e7326613f3f2
Elmahdi Amghar <eamghar@e2r7p14.1337.ma>
2024-04-21 14:21:20 +0100

    done with routing

--- React/src/components/LeaderBoardMain.tsx
+++ React/src/components/LeaderBoardMain.tsx
@@ -2,24 +2,23 @@ import Data from "../Data/LeaderBoardData.json";
 
 function getTop3() {
   const LeaderBoardData = Data;
-  // <h1>Level {LeaderBoardData.LeaderBoard[0].level}</h1>
   return (
     <div className="Top3">
       <div className="First">
         <img className="topImgs" src={LeaderBoardData.LeaderBoard[0].picture} alt="FirstPic" />
         <h1 className="toph1">{LeaderBoardData.LeaderBoard[0].username}</h1>
-        <div className="Coins"><h2>200 Coins</h2><h3 className="prize">Prize</h3></div>
+        <div className="Coins"><h2>200 Coins</h2></div>
       </div>
       <div className="SecondThird">
         <div className="Second">
           <img className="topImgs" src={LeaderBoardData.LeaderBoard[1].picture} alt="FirstPic" />
           <h1 className="toph1">{LeaderBoardData.LeaderBoard[1].username}</h1>
-          <div className="Coins"><h2>100 Coins</h2><h3 className="prize">Prize</h3></div>
+          <div className="Coins"><h2>100 Coins</h2></div>
         </div>
         <div className="Third">
           <img className="topImgs" src={LeaderBoardData.LeaderBoard[2].picture} alt="FirstPic" />
           <h1 className="toph1">{LeaderBoardData.LeaderBoard[2].username}</h1>
-          <div className="Coins"><h2>50 Coins</h2><h3 className="prize">Prize</h3></div>
+          <div className="Coins"><h2>50 Coins</h2></div>
         </div>
       </div>
     </div>
@@ -32,10 +31,14 @@ function getTheRest() {
     <div className="LeaderBoardRest">
       <ul>
         {LeaderBoardData.LeaderBoard.map((user) => (
-          <li key={user.id} id={user.id.toString()}>
-            <p>Username: {user.username}</p>
-            <p>Level: {user.level}</p>
-            <p>Coins: {user.coins}</p>
+          <li key={user.id} id={user.id.toString()} className={user.id % 2 == 0 ? "SpecialCase" : ""}>
+            <div className="idAndLine">
+              <h1>{user.id}</h1>
+              <div className="lineLi"/>
+            </div>
+            <img className="RestImgs" src={user.picture} alt="userPic" />
+            <h1>{user.username}</h1>
+            <h1>{user.level}</h1>
             <br />
           </li>
         ))}
@@ -56,43 +59,3 @@ function LeaderBoardMain() {
 
 export default LeaderBoardMain;
 
-// {
-//   "id": 5,
-//   "user_id": 3,
-//   "username": "Buda",
-//   "picture" : "https://source.unsplash.com/featured/300x201",
-//   "coins": 50,
-//   "level": 5,
-//   "first_name": "yahya",
-//   "last_name": "taqsi"
-// },
-// {
-//   "id": 6,
-//   "user_id": 3,
-//   "username": "zmoumni",
-//   "picture" : "https://source.unsplash.com/featured/300x201",
-//   "coins": 50,
-//   "level": 5,
-//   "first_name": "zakaria",
-//   "last_name": "moumni"
-// },
-// {
-//   "id": 7,
-//   "user_id": 3,
-//   "username": "jmou7i",
-//   "picture" : "https://source.unsplash.com/featured/300x201",
-//   "coins": 50,
-//   "level": 5,
-//   "first_name": "ayman",
-//   "last_name": "Msaoub"
-// },
-// {
-//   "id": 7,
-//   "user_id": 3,
-//   "username": "GamerGirl",
-//   "picture" : "https://source.unsplash.com/featured/300x201",
-//   "coins": 50,
-//   "level": 5,
-//   "first_name": "Yassmine",
-//   "last_name": "Makhlouf"
-// }
