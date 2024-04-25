524618b57f27b00d138884e11f5288c145d10e6a
Elmahdi Amghar <eamghar@e2r7p14.1337.ma>
2024-04-25 17:43:21 +0100

    working on history part of profile

--- React/src/components/History.tsx
+++ React/src/components/History.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import HistoryData from "../Data/HistoryData.json";
 
-function getScore(WholeStr: string, num: number) {
+function getScore(WholeStr: string) {
   let Str = WholeStr.split(":");
-  return Str[num];
+
+  return (
+    <>
+      <h1 className="UserScore">{Str[0] + " :"}</h1>
+      <h1 className="OppScore">&nbsp;{Str[1]}</h1>
+    </>
+  );
 }
 
 function History() {
   let Data = HistoryData;
-  let UserScore = 0;
-  let OpponentScore = 0;
   return (
     <div className="tableau">
       <table>
         <tr className="HistoryToolTipTable">
-          <th>Date</th>
-          <th>Oppow nent</th>
-          <th>Score</th>
-          <th>Game Mode</th>
-          <th>Length</th>
+          <th><h1>DATE</h1></th>
+          <th><h1>OPPONENT</h1></th>
+          <th><h1>SCORE</h1></th>
+          <th><h1>GAME MODE</h1></th>
+          <th><h1>LENGTH</h1></th>
         </tr>
         <div className="spacing"></div>
         {Data.History.map((element) => (
@@ -27,14 +31,11 @@ function History() {
               key={element.id}
               className={element.Result === "Won" ? "Won" : "Lost"}
             >
-              <td>{element.date}</td>
-              <td>{element.username}</td>
-              <td className="ScoreTd">
-                <h1 className="UserScore">{getScore(element.score, 0)} : </h1>
-                <h1 className="OppScore">{getScore(element.score, 1)}</h1>
-              </td>
-              <td>{element.GameMode}</td>
-              <td>{element.lenght}</td>
+              <td className="leftTd"><h1>{element.date}</h1></td>
+              <td><h1>{element.username}</h1></td>
+              <td className="ScoreTd">{getScore(element.score)}</td>
+              <td><h1>{element.GameMode}</h1></td>
+              <td className="rightTd"><h1>{element.lenght}</h1></td>
             </tr>
             <div className="spacing"></div>
           </>
