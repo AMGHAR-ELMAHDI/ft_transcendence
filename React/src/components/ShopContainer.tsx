c328d5fb7faf09cac94ea827b23e672fdc096bb3
Othman Chekairi <otchekai@e3r11p10.1337.ma>
2024-04-28 18:21:02 +0100

    Did Something

--- React/src/components/ShopContainer.tsx
+++ React/src/components/ShopContainer.tsx
@@ -1,16 +1,38 @@
 import SideBar from "./SideBar";
 import TopBar from "./TopBar";
 import FriendBar from "./FriendBar";
+import "../css/Shop.css";
 
 function ShopContainer() {
   return (
     <>
       <div className="AppClass">
         <SideBar />
-        <div className="main">
-          <TopBar />
-          <h1></h1>
-          
+        <div className="wrapper">
+          <div className="Headers-Shop">
+            <h1>SHOP</h1>
+            <h2>Gear Up Game On</h2>
+          </div>
+          <div className="ItemsToBuy">
+            <div className="Item-num-one">
+              <div className="Item-one-pic">
+                <div className="Item-one-name">
+                  <p>Of Cos Cheeky Breeky</p>
+                </div>
+              </div>
+              <div className="Item-one-value">
+                <div className="Item-one-button">
+                  <p>Buy</p>
+                </div>
+                <div className="Item-one-price-tag">
+                  <p>5$</p>
+                </div>
+              </div>
+            </div>
+            <div className="Item-num-two"></div>
+            <div className="Item-num-three"></div>
+            <div className="Item-num-four"></div>
+          </div>
         </div>
         <FriendBar />
       </div>
