5ce2c79d92f91662b009afbb45c57b66071cb638
Zakaria Makhkhas <zmakhkha@e2r7p6.1337.ma>
2024-05-06 19:48:42 +0100

    Cleaned up main

--- purshase/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import AchievementViewSet, ItemViewSet
-
-router = DefaultRouter()
-router.register('achievements', AchievementViewSet)
-router.register('items', ItemViewSet)
-
-urlpatterns = router.urls
--- userman/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def max_size_validator(file):
-	max_size_mb = 2
-	if (file.size > max_size_mb * 10**6):
-		raise ValidationError(f"File can not be larger that {max_size_mb}MB!")
