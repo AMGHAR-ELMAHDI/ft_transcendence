a8c40d0cc9ee94e6b5184a64c18314509388e493
Zakaria Makhkhas <zmakhkha@e2r7p6.1337.ma>
2024-05-07 19:00:59 +0100

    seeding database

---
+++ Django/userman/management/commands/data.sql
@@ -0,0 +1,25 @@
+INSERT INTO userman_item (type, name, price, path) VALUES
+('B', 'Ball 1', 10.00, 'ball_1.jpg'),
+('B', 'Ball 2', 10.00, 'ball_2.jpg'),
+('B', 'Ball 3', 10.00, 'ball_3.jpg'),
+('B', 'Ball 4', 10.00, 'ball_4.jpg'),
+('B', 'Ball 5', 10.00, 'ball_5.jpg'),
+
+('P', 'Paddle 1', 15.00, 'paddle_1.jpg'),
+('P', 'Paddle 2', 15.00, 'paddle_2.jpg'),
+('P', 'Paddle 3', 15.00, 'paddle_3.jpg'),
+('P', 'Paddle 4', 15.00, 'paddle_4.jpg'),
+('P', 'Paddle 5', 15.00, 'paddle_5.jpg'),
+
+('G', 'Background 1', 5.00, 'bg_1.jpg'),
+('G', 'Background 2', 5.00, 'bg_2.jpg'),
+('G', 'Background 3', 5.00, 'bg_3.jpg'),
+('G', 'Background 4', 5.00, 'bg_4.jpg'),
+('G', 'Background 5', 5.00, 'bg_5.jpg'),
+
+('A', 'Avatar 1', 20.00, 'avatar_1.jpg'),
+('A', 'Avatar 2', 20.00, 'avatar_2.jpg'),
+('A', 'Avatar 3', 20.00, 'avatar_3.jpg'),
+('A', 'Avatar 4', 20.00, 'avatar_4.jpg'),
+('A', 'Avatar 5', 20.00, 'avatar_5.jpg');
+
