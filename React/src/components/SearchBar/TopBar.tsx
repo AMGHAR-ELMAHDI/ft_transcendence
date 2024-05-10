4a2c414c2b580c583c3ba5a377c76a846fc0b3df
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2024-05-10 14:36:33 +0100

    fixed request problem in profile

--- React/src/components/SearchBar/TopBar.tsx
+++ React/src/components/SearchBar/TopBar.tsx
@@ -1,6 +1,6 @@
 import { IoNotificationsOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
-import React, { useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import { setAuthToken } from "../Utils/setAuthToken";
 import { useRecoilValue } from "recoil";
@@ -14,12 +14,14 @@ export function getPageName() {
 
 function TopBar() {
   const [data, setData] = React.useState<any>({});
+  const [gotData, setGotData] = React.useState(false);
 
   setAuthToken();
   const getData = async () => {
     try {
       const response = await axios.get("http://localhost:2500/player/me");
-      console.log(response.data);
+      setGotData(true);
+      console.log("--------------HERE-------------");
       setData(response.data);
     } catch (error) {
       console.log(error);
@@ -28,24 +30,13 @@ function TopBar() {
 
   const Logged = useRecoilValue(IsLogged);
 
-  useEffect(() => {
-    if (Logged == true) getData();
-  }, []);
-
-  console.log("TOPBAR user: " + data.username);
+  if (Logged == true && gotData === false) getData();
 
   const obj = {
     username: data.username ? data.username : "Dawdaw",
-    first_name: data.first_name ? data.first_name : "First",
-    last_name: data.last_name ? data.last_name : " Last",
     avatar: data.avatar ? data.avatar : "/bacharG.svg",
     friends: data.friends ? data.friends : [0],
-    win_rate: data.win_rate ? data.win_rate : 0,
     level: data.level ? data.level : 0,
-    achievements_rate: data.achievements_rate ? data.achievements_rate : 0,
-    achievements: data.achievements ? data.achievements : [0],
-    items: data.items ? data.items : [0],
-    games: data.games ? data.games : [0],
   };
   let print = <h1>Good Evening,</h1>;
   let username = <h1 id="nickName">{obj.username}</h1>;
