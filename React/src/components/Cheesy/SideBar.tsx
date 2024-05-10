b9d2defdfabc6b52f8efcfeacb28c9e7cd27ed23
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2024-05-10 17:45:09 +0100

    fixed logout bug

--- React/src/components/Cheesy/SideBar.tsx
+++ React/src/components/Cheesy/SideBar.tsx
@@ -6,16 +6,23 @@ import LogOutSideBar from "../SideBar/LogOutSideBar";
 import LogoutPopUp from "../SideBar/Logout";
 import { IoGameControllerOutline } from "react-icons/io5";
 import { PiChatCircleTextLight } from "react-icons/pi";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link} from "react-router-dom";
-
+import AcessToken from "../../Atoms/AccessToken";
+import IsLogged from "../../Atoms/IsLogged";
+import { useRecoilState } from "recoil";
 
 
 function SideBar() {
     const [showLogoutPopup, setShowLogoutPopup] = useState(false);
+    const [tokenValue, setTokenValue] = useRecoilState(AcessToken);
+    const [Logged, setLogged] = useRecoilState(IsLogged);
 
     const handleConfirmLogout = () => {
       setShowLogoutPopup(false);
+      setTokenValue("");
+      setLogged(false);
+      console.log("USER LOGGED OUT");
     };
 
     const handleCancelLogout = () => {
