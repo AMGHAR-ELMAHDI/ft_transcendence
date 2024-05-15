52e7e720f07c74e87842e6ee5453a1409023c802
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2024-05-15 22:52:01 +0100

    Working on player profile

--- React/src/components/Cheesy/ProfileLayout.tsx
+++ React/src/components/Cheesy/ProfileLayout.tsx
@@ -1,13 +1,20 @@
 import SideBar from "./SideBar";
 import TopBar from "../SearchBar/TopBar";
-import Profile from "./Profile";
-import ProfileMain from "./ProfileMain";
 import FriendBar from "./FriendBar";
 import { Outlet, useParams } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import Users from "../../Atoms/UsersName";
+import { useEffect } from "react";
 
 function ProfileLayout() {
+  const [data, setData] = useRecoilState(Users);
+
   const { username } = useParams();
-  console.log("Layout: " + username);
+
+  useEffect(() => {
+    if (username) setData(username);
+    console.log("Layout: " + data);
+  }, [username, data, setData]);
   return (
     <>
       <div className="AppClass">
--- React/src/components/Cheesy/Users.tsx
+++ React/src/components/Cheesy/Users.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import UsersName from "../../Atoms/UsersName";
+
 
 function Users() {
-  const { username } = useParams();
+  const username: string = useRecoilValue(UsersName);
   console.log("Users: " + username);
 
   return <div>{username}</div>;
