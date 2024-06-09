b476587a7c418c7207595a60c634c31583c389c3
AMGHAR ELMAHDI <amgharmahdi1@gmail.com>
2024-06-09 22:31:01 +0100

    Fixed No Connection from Server Error

--- React/src/components/Cheesy/SideBar.tsx
+++ React/src/components/Cheesy/SideBar.tsx
@@ -7,43 +7,122 @@ import LogoutPopUp from "../SideBar/Logout";
 import { IoGameControllerOutline } from "react-icons/io5";
 import { PiChatCircleTextLight } from "react-icons/pi";
 import { useState } from "react";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AcessToken from "../../Atoms/AccessToken";
 import IsLogged from "../../Atoms/IsLogged";
 import { useRecoilState } from "recoil";
 
+import Drawer from "react-modern-drawer";
+
+import "react-modern-drawer/dist/index.css";
+
+function SideBarLinks() {
+  return (
+    <div className="sidebar-links">
+      <Link to={"/"}>
+        <Home />
+      </Link>
+      <Link to={"/chat"}>
+        <PiChatCircleTextLight
+          color="#757889"
+          className={
+            window.location.pathname === "/chat"
+              ? "sidebarImgsBlue"
+              : "sidebarImgs"
+          }
+        />
+      </Link>
+      <Link to={"/game"}>
+        <IoGameControllerOutline
+          className={
+            window.location.pathname === "/game"
+              ? "sidebarImgsBlue"
+              : "sidebarImgs"
+          }
+        />
+      </Link>
+      <Link to={"/leaderboard"}>
+        <div className="LeaderSide">
+          <Leaderboard />
+        </div>
+      </Link>
+      <Link to={"/shop"}>
+        <Shop />
+      </Link>
+      <Link to={"/profile"}>
+        <ProfileSideBar />
+      </Link>
+    </div>
+  );
+}
+
+function DrawerLinks() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="DrawerLinks">
+      <h1 onClick={() => navigate("/")}>Home</h1>
+      <h1 onClick={() => navigate("/chat")}>Chat</h1>
+      <h1 onClick={() => navigate("/game")}>Game</h1>
+      <h1 onClick={() => navigate("/leaderboard")}>Leaderboard</h1>
+      <h1 onClick={() => navigate("/shop")}>Shop</h1>
+      <h1 onClick={() => navigate("/profile")}>Profile</h1>
+    </div>
+  );
+}
 
 function SideBar() {
-    const [showLogoutPopup, setShowLogoutPopup] = useState(false);
-    const [tokenValue, setTokenValue] = useRecoilState(AcessToken);
-    const [Logged, setLogged] = useRecoilState(IsLogged);
+  const [showLogoutPopup, setShowLogoutPopup] = useState(false);
+  const [tokenValue, setTokenValue] = useRecoilState(AcessToken);
+  const [Logged, setLogged] = useRecoilState(IsLogged);
 
-    const handleConfirmLogout = () => {
-      setShowLogoutPopup(false);
-      setTokenValue("");
-      localStorage.removeItem("token");
-      setLogged(false);
-    };
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
 
-    const handleCancelLogout = () => {
-      setShowLogoutPopup(false);
-    };
-    const navigate = useNavigate();
+  const handleConfirmLogout = () => {
+    setShowLogoutPopup(false);
+    setTokenValue("");
+    localStorage.removeItem("token");
+    setLogged(false);
+  };
+
+  const handleCancelLogout = () => {
+    setShowLogoutPopup(false);
+  };
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    // navigate("/");
+  };
 
   return (
     <div id="SideBar">
-      <img onClick={() => navigate("/")} id="logo" src="/logo.svg" alt="logo" />
-      <div className="sidebar-links">
-        <Link to={"/"}> <Home /></Link>
-        <Link to={"/chat"}> <PiChatCircleTextLight color="#757889" className={ window.location.pathname === "/chat" ? "sidebarImgsBlue" : "sidebarImgs" }/></Link>
-        <Link to={"/game"}> <IoGameControllerOutline className={ window.location.pathname === "/game" ? "sidebarImgsBlue" : "sidebarImgs"}/></Link>
-        <Link to={"/leaderboard"}><div className="LeaderSide"><Leaderboard /></div></Link>
-        <Link to={"/shop"}><Shop /></Link>
-        <Link to={"/profile"}><ProfileSideBar /></Link>
+      <div className="absolute">
+        <Drawer
+          open={isOpen}
+          onClose={toggleDrawer}
+          direction="left"
+          className="Drawer"
+        >
+          <DrawerLinks />
+        </Drawer>
       </div>
-      <div className="SideBottom" onClick={() => (setShowLogoutPopup(!showLogoutPopup))}>
+      <img onClick={toggleDrawer} id="logo" src="/logo.svg" alt="logo" />
+      <SideBarLinks />
+      <div
+        className="SideBottom"
+        onClick={() => setShowLogoutPopup(!showLogoutPopup)}
+      >
         <LogOutSideBar />
-        {showLogoutPopup && <LogoutPopUp onConfirm={handleConfirmLogout} onCancel={handleCancelLogout}/>}
+        {showLogoutPopup && (
+          <LogoutPopUp
+            onConfirm={handleConfirmLogout}
+            onCancel={handleCancelLogout}
+          />
+        )}
       </div>
     </div>
   );
