c2b207754ad3c26e12a9b6f42f06bc5b2b2876bb
zmakhkha <jcaladic@gmail.com>
2024-07-06 17:16:21 +0100

    [Back-end]discord Oauth2 works properly

--- Django/discordOauth/views.py
+++ Django/discordOauth/views.py
@@ -2,34 +2,78 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect
 import requests
+from django.db.models import F
+from django.conf import settings
+from userman.models import Player
+from django.utils.crypto import get_random_string
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+
+
 
 def home(HttpRequest):
     return(JsonResponse({'msg' : 'hi there'}))
 
-uri='https://discord.com/oauth2/authorize?client_id=1259091120730669106&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A2500%2Fdiscord%2Flogin%2Fredirect&scope=identify'
 def discord_login(HttpRequest):
-	return redirect(uri)
+	return redirect(settings.URI)
 
-def discord_redirect(request : HttpRequest):
+
+# Function to handle Discord login redirect
+def discord_redirect(request: HttpRequest):
     code = request.GET.get('code')
-    
-    getData(code=code)
-    
-    return JsonResponse({'msg' : code})
+    if code:
+        access_token = exchange_code(code)
+        if access_token:
+            user_info_response = requests.get("https://discord.com/api/v6/users/@me", headers={
+                "Authorization": f"Bearer {access_token}"
+            })
+            user_info = user_info_response.json()
+            discord_id = user_info.get('id')
+            email = user_info.get('email')
+            username = user_info.get('username')
 
-def getData(code : str):
+            user = Player.objects.filter(email=email).first()
+            if not user:
+                random_password = get_random_string(length=12)
+                user = Player.objects.create(
+                    email=email,
+                    username=username
+                )
+                user.set_password(random_password)
+                user.save()
+                
+            user = Player.objects.filter(email=email).first()
+            if user is not None:
+                login(request, user)
+                refresh = RefreshToken.for_user(user)
+                return JsonResponse({
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                })
+            else:
+                return JsonResponse({'error': 'Authentication failed'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return JsonResponse({'error': 'Failed to exchange code for access token'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return JsonResponse({'error': 'No code parameter received'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+def exchange_code(code: str):
     data = {
-		"client_id": "1259091120730669106",
-		"client_secret" : "oKkTjjaDLetyGb-H1Lukn7tq-GPeroj0",
-		"grant_type" : "authorization_code",
-		"code" : code,
-		"redirect_uri" : "http://localhost:2500/discord/login/redirect",
-		"scope" : "identify",
-	}
+        "client_id": settings.CLIENT_ID,
+		"client_secret" : settings.CLIENT_SECRET,
+        "grant_type": settings.GRANT_TYPE,
+        "code": code,
+        "redirect_uri": settings.REDIRECT_URI,
+        "scope": settings.SCOPE,
+    }
     headers = {
-		'Content-type': 'application/x-www-form-urlencoded'
-	}
+        'Content-type': 'application/x-www-form-urlencoded'
+    }
     response = requests.post("https://discord.com/api/oauth2/token", data=data, headers=headers)
-    print(response)
     credentials = response.json()
-    print(credentials)
+    access_token = credentials.get('access_token')
+    return access_token
--- Django/myChat/settings.py
+++ Django/myChat/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django_otp.plugins.otp_static',
     'django_otp.plugins.otp_totp',
     'two_factor',
+    'discordOauth',
 ]
 
 MIDDLEWARE = [
@@ -205,3 +206,13 @@ LOGIN_REDIRECT_URL = 'http://localhost:2500/auth/jwt/create'
 
 
 LOGIN_URL = 'two_factor:login'
+
+
+# Discord Oauth
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+GRANT_TYPE = os.getenv('GRANT_TYPE')
+REDIRECT_URI = os.getenv('REDIRECT_URI')
+SCOPE = os.getenv('SCOPE')
+URI = os.getenv('URI')
