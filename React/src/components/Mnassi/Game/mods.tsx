b5eb635b9026bcac8b773d4b1be7e38c36797d40
Mohammed Nassi <mnassi@e3r3p3.1337.ma>
2024-07-12 13:28:45 +0100

    changed some files in gitignore

--- React/src/components/Mnassi/Game/mods.tsx
+++ React/src/components/Mnassi/Game/mods.tsx
@@ -6,12 +6,14 @@ function mods() {
 
 	})
 	return (
-		<div className="_mods">
-			<img className='Imods' id='0' src='/first.svg'></img>
-			<img className='Imods' id='1' src='/sec.svg'></img>
-			<img className='Imods' id='2' src='/third.svg'></img>
-			<img className='Imods' id='3' src='/forth.svg'></img>
-		</div>
+		// <div className="mod_Cont">
+			<div className="_mods">
+				<img className='Imods' id='0' src='/first.svg'></img>
+				<img className='Imods' id='1' src='/sec.svg'></img>
+				<img className='Imods' id='2' src='/third.svg'></img>
+				<img className='Imods' id='3' src='/forth.svg'></img>
+			</div>
+		// </div>
 	);
 }
 
